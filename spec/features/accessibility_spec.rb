81a60ca7651c17d0d877e9b29c705476200b2c4a
Taylor Yamashita <taylory@princeton.edu>
2022-11-08 14:42:34 -0500

    Create accessibility specs

---
+++ spec/features/accessibility_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "accessibility", type: :feature, js: true do
+  context "home page" do
+    it "complies with WCAG" do
+      visit "/"
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa)
+    end
+  end
+end
