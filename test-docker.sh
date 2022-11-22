09d15835b28a0ab2878e8dd46cb15f9e586deec9
mccalluc <mccalluc@users.noreply.github.com>
2022-11-22 09:46:57 -0500

    debug missing output dir

--- test-docker.sh
+++ test-docker.sh
@@ -5,6 +5,18 @@ TAG=rails-template
 NEW_DIR=template-new
 OLD_DIR=template-dir
 
+# debugging...
+echo "NEW_DIR? $NEW_DIR"
+echo "docker build supports --output?"
+docker help build
+echo "write privs?"
+mkdir this-is-a-test
+touch this-is-a-test/123.txt
+echo "ls cwd?"
+ls
+echo "ls new dir?"
+ls this-is-a-test
+
 docker build docker-context --tag $TAG --output $NEW_DIR
 echo 'Docker build successful! To connect:'
 echo "  docker run -it $TAG"
