ddaf422c7b76d655add9081c21853d816c33dc37
mccalluc <mccalluc@users.noreply.github.com>
2022-11-22 12:16:50 -0500

    account for gitignored files

--- test-docker.sh
+++ test-docker.sh
@@ -14,5 +14,15 @@ DOCKER_BUILDKIT=1 docker build docker-context --tag $TAG --output $NEW_DIR
 echo 'Docker build successful! To connect:'
 echo "  docker run -it $TAG"
 
+# These files are in the docker image, but are git-ignored or don't belong in the template.
+cd $NEW_DIR
+rm -rf .git \
+       .ruby-version \
+       config/master.key \
+       log/development.log \
+       tmp/development_secret.txt
+cd -
+
+
 diff -r $OLD_DIR $NEW_DIR
 echo "Yay! No difference between Docker output and $OLD_DIR."
