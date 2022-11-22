001ae00f7efdab57f58353db3ad69c4ea083783a
mccalluc <mccalluc@users.noreply.github.com>
2022-11-21 22:18:32 -0500

    tool versions

---
+++ test-docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit
+
+TAG=rails-template
+NEW_DIR=template-new
+OLD_DIR=template-dir
+
+docker build docker-context --tag $TAG --output $NEW_DIR
+echo 'Docker build successful! To connect:'
+echo "  docker run -it $TAG"
+
+diff -r $OLD_DIR $NEW_DIR
+echo "Yay! No difference between Docker output and $OLD_DIR"
