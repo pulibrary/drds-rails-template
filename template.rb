66a908c19ea66912c34f541217020b6814f0f3bd
Taylor Yamashita <taylory@princeton.edu>
2023-03-27 16:02:05 -0400

    update add_gem commands

--- template.rb
+++ template.rb
@@ -1,42 +1,42 @@
 # frozen_string_literal: true
 
 def add_gems
-  gem "bundler", "2.3.18"
-  gem "rails", "~> 6.1.3", ">= 6.1.3.2"
-  gem "pg"
-  gem "puma", "~> 5.0"
-  gem "sass-rails", ">= 6"
-  gem "webpacker", "~> 5.0"
-  gem "turbolinks", "~> 5"
-  gem "jbuilder", "~> 2.7"
-  gem "bootsnap", ">= 1.4.4", require: false
+  add_gem "bundler", "2.3.18"
+  add_gem "rails", "~> 6.1.3", ">= 6.1.3.2"
+  add_gem "pg"
+  add_gem "puma", "~> 5.0"
+  add_gem "sass-rails", ">= 6"
+  add_gem "webpacker", "~> 5.0"
+  add_gem "turbolinks", "~> 5"
+  add_gem "jbuilder", "~> 2.7"
+  add_gem "bootsnap", ">= 1.4.4", require: false
   # group :development, :test do
-  gem "bixby"
-  gem "pry-byebug"
-  gem "pry-rails"
-  gem "rspec-rails", "~> 5.0.0"
+  add_gem "bixby"
+  add_gem "pry-byebug"
+  add_gem "pry-rails"
+  add_gem "rspec-rails", "~> 5.0.0"
   # group :development do
-  gem "web-console", ">= 4.1.0"
-  gem "listen", "~> 3.3"
-  gem "rack-mini-profiler", "~> 2.0"
-  gem "spring"
-  gem "capistrano"
-  gem "capistrano-passenger"
-  gem "capistrano-rails"
-  gem "capistrano-rails-console"
+  add_gem "web-console", ">= 4.1.0"
+  add_gem "listen", "~> 3.3"
+  add_gem "rack-mini-profiler", "~> 2.0"
+  add_gem "spring"
+  add_gem "capistrano"
+  add_gem "capistrano-passenger"
+  add_gem "capistrano-rails"
+  add_gem "capistrano-rails-console"
   # group :test do
-  gem "axe-core-rspec"
-  gem "capybara", ">= 3.26"
-  gem "selenium-webdriver"
-  gem "simplecov", require: false
-  gem "webdrivers"
+  add_gem "axe-core-rspec"
+  add_gem "capybara", ">= 3.26"
+  add_gem "selenium-webdriver"
+  add_gem "simplecov", require: false
+  add_gem "webdrivers"
   # other
-  gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-  gem "matrix"
-  gem "net-imap", require: false
-  gem "net-pop", require: false
-  gem "net-smtp", require: false
-  gem "strscan", "3.0.1"
+  add_gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+  add_gem "matrix"
+  add_gem "net-imap", require: false
+  add_gem "net-pop", require: false
+  add_gem "net-smtp", require: false
+  add_gem "strscan", "3.0.1"
 end
 
 add_gems
