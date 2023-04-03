e952a563c03b263bdb94b7fc6e84ae4d23b8f1bc
Taylor Yamashita <taylory@princeton.edu>
2023-04-03 13:59:31 -0400

    change quotes, fix formatting in Gemfile updates, add cap gem injections

--- template.rb
+++ template.rb
@@ -4,25 +4,23 @@
 def update_gemfile
 
   # Replace sqlite3 gem with pg
-  gsub_file "Gemfile", 'gem "sqlite3', 'gem "pg"'
+  gsub_file "Gemfile", "gem 'sqlite3', \'~> 1.4\'", "gem 'pg'"
   gsub_file "Gemfile", "# Use sqlite3 as the database for Active Record", "# Use PostgreSQL as the database"
 
   # Update :development, :test group
-  inject_into_file "Gemfile", "gem \"bixby\"\n", after: "gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]\n"
-  inject_into_file "Gemfile", "gem \"pry-byebug\"\n", after: "gem \"bixby\"\n"
-  inject_into_file "Gemfile", "gem \"pry-rails\"\n", after: "gem \"pry-byebug\"\n"
-  inject_into_file "Gemfile", "gem \"rspec-rails\"\n", after: "gem \"pry-rails\"\n"
-  gsub_file "Gemfile", "# Call 'byebug' anywhere in the code to stop execution and get a debugger console\n",''
-  gsub_file "Gemfile", "gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]\n",''
+  inject_into_file "Gemfile", "  gem 'bixby'\n", after: "gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]\n"
+  inject_into_file "Gemfile", "  gem 'pry-byebug'\n", after: "gem 'bixby'\n"
+  inject_into_file "Gemfile", "  gem 'pry-rails'\n", after: "gem 'pry-byebug'\n"
+  inject_into_file "Gemfile", "  gem 'rspec-rails', '~> 5.0'\n", after: "gem 'pry-rails'\n"
+  gsub_file "Gemfile", "  # Call 'byebug' anywhere in the code to stop execution and get a debugger console\n",''
+  gsub_file "Gemfile", "  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]\n",''
   
-  
-
   # Update :development group
-  # inject_into_file 'Gemfile', '\n# Use Capistrano for deployment', after: 'group :development, :test do'
-  # inject_into_file 'Gemfile', "\ngem \"capistrano\"", after: '# Use Capistrano for deployment'
-  # inject_into_file 'Gemfile', '\ngem "capistrano-passenger"', after: 'gem "capistrano"'
-  # inject_into_file 'Gemfile', '\ngem "capistrano-rails"', after: 'gem "capistrano-passenger"'
-  # inject_into_file 'Gemfile', '\ngem "capistrano-rails-console"', after: 'gem "capistrano-rails"'
+  inject_into_file "Gemfile", "  # Use Capistrano for deployment\n", after: "gem 'spring'\n"
+  inject_into_file "Gemfile", "  gem 'capistrano'\n", after: "# Use Capistrano for deployment\n"
+  inject_into_file "Gemfile", "  gem 'capistrano-passenger'\n", after: "gem 'capistrano'\n"
+  inject_into_file "Gemfile", "  gem 'capistrano-rails'\n", after: "gem 'capistrano-passenger'\n"
+  inject_into_file "Gemfile", "  gem 'capistrano-rails-console'\n", after: "gem 'capistrano-rails'\n"
 
   # gem_group :test do
   #   # Automate accessibility testing
